dd60e9b19207d16baade034daf3984219ccbe95c
tim.coates <tim.coates@gmail.com>
2022-10-12 09:52:49 +0100

    Added more JWT builders into utils.

--- steps/utils.js
+++ steps/utils.js
@@ -106,12 +106,114 @@ function bodyNotYetValid() {
 	return base64url(JSON.stringify(clone));
 }
 
+function bodyEmptyPayloads() {
+	let clone = structuredClone(bodyStructure);
+	clone.payloads = [];
+	return base64url(JSON.stringify(clone));
+}
 
+function bodyNPayloads(n) {
+	let clone = structuredClone(bodyStructure);
+	clone.payloads = [];
+	for (let x = 1; x <= n; x++) {
+		let item = {
+			"id": x.toString(),
+			"payload": "PFNpZ25lZEluZm8+PENhbm9uaWNhbGl6YXRpb25NZXRob2QgQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzEwL3htbC1leGMtYzE0biMiPjwvQ2Fub25pY2FsaXphdGlvbk1ldGhvZD48U2lnbmF0dXJlTWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnI3JzYS1zaGExIj48L1NpZ25hdHVyZU1ldGhvZD48UmVmZXJlbmNlPjxUcmFuc2Zvcm1zPjxUcmFuc2Zvcm0gQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzEwL3htbC1leGMtYzE0biMiPjwvVHJhbnNmb3JtPjwvVHJhbnNmb3Jtcz48RGlnZXN0TWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnI3NoYTEiPjwvRGlnZXN0TWV0aG9kPjxEaWdlc3RWYWx1ZT45UUh6UWFGaTJtL2pnNjFYa3REQzdMa3VoRFU9PC9EaWdlc3RWYWx1ZT48L1JlZmVyZW5jZT48L1NpZ25lZEluZm8+"
+		};
+		clone.payloads.push(item);
+	}
+	return base64url(JSON.stringify(clone));
+}
+
+
+function signature() {
+	return "eK6ak8YjcG8_k5Q0QH1fcDjmoXo-SB3ocNPW3rPLDR99hMv2EdFYulZXk3fPhL4wYaOut16jL0yTKpcfKrNFTNCYW6d7dPF5E984reqmbFqs7ic4PBIZfv8tUHYAgUBE2LT_g60PJdrME5SXcng12h1FXePWBbiAQQKAWBxLv7_aWr0HC7x9CudTr9jF8IdirFN7EpxIph73FE7j0uLmUEME-Fb2Sloi4UvtHBXVoc7KASDi9XAVngInUgsg5G8w_1642zfmbZsKJyemEYRGMHGVGmzoM_pycBxJ8a8BI3HNL_4ZpKEwUXKul09AwO2dzG9EWXTMKJNVA2oawehE4KruI3yvghQjfzq6ta9oyIJ66AKSQQd833_UEEhfLsDFIEx1Im2aVytwmSnRnpDomPuIzpZe7EdX9OR7AS7D1eGTFPX8xANoV7WM5dyrOyP5Iwwc6wztfUEbFmCxaj-qRsCE6YvCX3Pqi6XBXYwpE2x0LLX39a8kOZasAsJBWvSsGIUEBx9xt4g7wl1GbSSFZtr_SogcKGDu-Jz_4LIb5b_VH8lhYCsPxVHLaSf8wKAfecYomZO-Q8TW21B4TVO-M2pPa8Ht5BG5T2XcC-yPm66oF7OUze0uNaRhMXXPRtSYbXxVOlZB7VtywN5MFH4WgXjAepw0pQ9EcVVVAGFJjD0";
+}
+
+function valid() {
+	return header + "." + body + "." + signature;
+}
+
+function RS1() {
+	return headerRS1 + "." + body + "." + signature;
+}
+
+function noALG() {
+	return headerNoALG + "." + body + "." + signature;
+}
+
+function noTYP() {
+	return headerNoTYP + "." + body + "." + signature;
+}
+
+function noKID() {
+	return headerNoKID + "." + body + "." + signature;
+}
+
+function noPayloads() {
+	return header + "." + bodyNoPayloads + "." + signature;
+}
+
+function noAlgorithm() {
+	return header + "." + bodyNoAlgorithm + "." + signature;
+}
+
+function noIAT() {
+	return header + "." + bodyNoIAT + "." + signature;
+}
+
+function noEXP() {
+	return header + "." + bodyNoEXP + "." + signature;
+}
+
+function noAUD() {
+	return header + "." + bodyNoAUD + "." + signature;
+}
+
+function noISS() {
+	return header + "." + bodyNoISS + "." + signature;
+}
+
+function noSUB() {
+	return header + "." + bodyNoSUB + "." + signature;
+}
+
+function expired() {
+	return header + "." + bodyExpired + "." + signature;
+}
+
+function notYetValid() {
+	return header + "." + bodyNotYetValid + "." + signature;
+}
+
+function emptyPayloads() {
+	return header + "." + bodyEmptyPayloads + "." + signature;
+}
+
+function manyPayloads(n) {
+	return header + "." + bodyNPayloads(n) + "." + signature;
+}
+
+function noSignature() {
+	return header + "." + body + ".";
+}
 
 module.exports = {
-	header: header,
-	headerRS1: headerRS1,
-	headerNoALG: headerNoALG,
-	headerNoTYP: headerNoTYP,
-	headerNoKID: headerNoKID
+	valid: valid,
+	RS1: RS1,
+	noALG: noALG,
+	noTYP: noTYP,
+	noKID: noKID,
+	noPayloads: noPayloads,
+	noAlgorithm: noAlgorithm,
+	noIAT: noIAT,
+	noEXP: noEXP,
+	noAUD: noAUD,
+	noISS: noISS,
+	noSUB: noSUB,
+	expired: expired,
+	notYetValid: notYetValid,
+	emptyPayloads: emptyPayloads,
+	manyPayloads: manyPayloads,
+	noSignature: noSignature
 };
